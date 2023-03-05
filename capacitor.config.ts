9150a20d69fba7b4e7d99bf04ab266caa002d58d
nasser <nissa@transnegrd.com>
2023-03-05 09:48:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea6_idam',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
